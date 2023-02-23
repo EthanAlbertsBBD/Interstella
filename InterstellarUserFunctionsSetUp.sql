66e14bf50f57aa5bab8e3b176c0ee5fa780a2f47
Ethan Alberts <ethana@bbd.co.za>
2023-02-23 16:21:54 +0200

    Updated user defined functions

--- InterstellarUserFunctionsSetUp.sql
+++ InterstellarUserFunctionsSetUp.sql
@@ -1,26 +1,33 @@
-CREATE FUNCTION dbo.udfGetUserCurrentEntity (@Username varchar)
-RETURNS int AS
-BEGIN
-	DECLARE @CosmitcEntity int
-
-	SELECT @CosmitcEntity = CurrentEntity FROM Users
-	WHERE Username = @Username
-	
-	RETURN @Username
-END
-
-GO
-
-CREATE FUNCTION dbo.udfGetChildrenOfACosmicEntity (@EntityID int)
+CREATE FUNCTION dbo.udfGetChildrenOfACosmicEntity(@EntityID int)
 RETURNS table AS	
 	RETURN (SELECT Name, EntityID FROM CosmicEntities
 	WHERE SuperEntityID = @EntityID)
 
 GO
 
-CREATE FUNCTION dbo.udfGetParentCosmicEntity (@EntityID int)
-RETURNS table AS	
-	RETURN (SELECT Name, EntityID FROM CosmicEntities
-	WHERE @EntityID = @EntityID)
+CREATE FUNCTION dbo.udfGetUser(@Username varchar)
+RETURNS table AS
+	RETURN (SELECT CharacterName,
+	CurrentEntity,
+	CustomiseID FROM Users
+	WHERE Username = @Username)
+GO
 
+CREATE FUNCTION dbo.udfGetUserCurrentEntity(@EntityID varchar)
+RETURNS table AS
+	RETURN (SELECT CosmicEntities.EntityID,
+	CosmicEntities.Name,
+	CosmicEntities.DetailDescription,
+	CosmicEntities.SuperEntityID,
+	Properties.Size,
+	Properties.Mass,
+	Properties.Temperature,
+	Properties.DistanceFromEarth,
+	Properties.EntityTypeID
+	FROM CosmicEntities
+	INNER JOIN (Properties
+		INNER JOIN EntityTypes
+		ON Properties.EntityTypeID = EntityTypes.EntityTypeID)
+	ON CosmicEntities.EntityID = Properties.PropertyID
+	WHERE CosmicEntities.EntityID = @EntityID)
 GO
